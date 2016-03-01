6f2f670993b2a76955797858ec8f2290a3590f4c
Ecl1pce <vvaskez@yandex.ru>
2016-03-01 21:30:11 +0300

    Core interface

--- sources/Core/gameapi.h
+++ sources/Core/gameapi.h
@@ -8,6 +8,14 @@ class CORESHARED_EXPORT GameAPI
 
 public:
     GameAPI();
+    void GetField();
+    int StartGame(int difficulty);
+    void OpenCell(int x, int y);
+    void SetRedFlag(int x, int y);
+    void SetQuestion(int x, int y);
+    void GetStatus();
+    void SaveGame();
+
 };
 
 #endif // GAMEAPI_H
